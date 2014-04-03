97de03f425bed6e202b2db01e18b868b8cb534de
cgi-soa-daniela <soamc2014!>
2014-04-03 17:33:51 +0200

    ..

---
+++ src/main/java/com/cgi/soa/masterclass/clickpay/model/FeeEntity.java
@@ -0,0 +1,59 @@
+package com.cgi.soa.masterclass.clickpay.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+/**
+ * Entity implementation class for Entity: Fee
+ *
+ */
+@Entity
+@Table
+public class FeeEntity implements Serializable {
+
+	   
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int Id;
+	
+	@JoinColumn
+	private TransactionEntity transaction;
+	
+	@Column
+	private float amount;
+	
+	private static final long serialVersionUID = 1L;
+
+	public FeeEntity() {
+	}   
+	
+	public int getId() {
+		return this.Id;
+	}
+
+	public void setId(int Id) {
+		this.Id = Id;
+	}   
+	public TransactionEntity getTransaction() {
+		return this.transaction;
+	}
+
+	public void setTransaction(TransactionEntity transaction) {
+		this.transaction = transaction;
+	}   
+	public float getAmount() {
+		return this.amount;
+	}
+
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+   
+}
---
+++ src/main/java/com/cgi/soa/masterclass/clickpay/model/TransactionEntity.java
@@ -0,0 +1,95 @@
+package com.cgi.soa.masterclass.clickpay.model;
+
+import java.io.Serializable;
+
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.cgi.soa.masterclass.clickpay.faces.User;
+
+
+
+/**
+ * Entity implementation class for Entity: Transaction
+ *
+ */
+@Entity
+@Table
+public class TransactionEntity implements Serializable {
+
+	   
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int Id;
+	
+	@Column
+	private User sender;
+	
+	@Column
+	private User recipient;
+	
+	@Column
+	private Date transdate;
+	
+	@Column
+	private String purpose;
+	
+	@Column
+	private float amount;
+	
+	private static final long serialVersionUID = 1L;
+
+	public TransactionEntity() {
+	}   
+	
+	public int getId() {
+		return this.Id;
+	}
+
+	public void setId(int Id) {
+		this.Id = Id;
+	}   
+	public User getSender() {
+		return this.sender;
+	}
+
+	public void setSender(User sender) {
+		this.sender = sender;
+	}   
+	public User getRecipient() {
+		return this.recipient;
+	}
+
+	public void setRecipient(User recipient) {
+		this.recipient = recipient;
+	}   
+	public Date getTransdate() {
+		return this.transdate;
+	}
+
+	public void setTransdate(Date transdate) {
+		this.transdate = transdate;
+	}   
+	public String getPurpose() {
+		return this.purpose;
+	}
+
+	public void setPurpose(String purpose) {
+		this.purpose = purpose;
+	}   
+	public float getAmount() {
+		return this.amount;
+	}
+
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+   
+}
